63c5e4a238beba4b920aefc09de8e9149cb32d84
Peter Claver Amobila <57482239+Limitless-Kode@users.noreply.github.com>
2023-09-08 14:11:34 +0100

    changed baseURL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://limitless-kode.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/brn-whitepaper/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
